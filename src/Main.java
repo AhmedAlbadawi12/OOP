e93f7111704d2c2d7968049b76f4ce5e8513509c
AhmedAlbadawi12 <medogaming5757@gmail.com>
2023-12-13 19:52:37 +0200

    mohaaaaaaab

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Ahmed"); // GOOD MORNING AHMED
-        System.out.println("HP");
+        System.out.println("HP"); // good evining mohab
     }
 }
