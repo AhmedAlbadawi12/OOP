d5b1658d207a60e1053d9b6b3ba43527b046081f
MohabMohamed222 <mohabmansour2004@gmail.com>
2023-12-13 19:54:42 +0200

    Mohab jhdsfkjs

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Ahmed"); // GOOD MORNING AHMED
         System.out.println("HP"); // good evining mohab
+
+        for(int i=0;i<50;i++)
+            System.out.println("AHMED ");
     }
 }
