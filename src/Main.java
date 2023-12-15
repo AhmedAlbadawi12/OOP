d1937f80b23665c902514f48e83c3c93f1007eb1
MohabMohamed222 <mohabmansour2004@gmail.com>
2023-12-15 13:44:24 +0200

    Mohab

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ public class Main {
         System.out.println("Hello Ahmed"); // GOOD MORNING AHMED
         System.out.println("HP"); // good evining mohab
 
-        for(int i=0;i<3;i++){
+        /*for(int i=0;i<3;i++){
             System.out.println("AHMED كلب البحر");}
 
-        for(int i=0;i<3;i++){
-            System.out.println("مهاب عروسه النيل ");}
+        /*for(int i=0;i<3;i++){
+            System.out.println("مهاب عروسه النيل ");}*/
+
+        System.out.println("حياتي ما اجمل فيها بحس طعمة من تغيرة");
 
     }
 }
